dd8fff1fa48f61efd7848948a09a36c16f6de6f1
Sharaneshwar <punjalsharaneshwar@gmail.com>
2024-08-05 11:07:57 +0530

    added assignment 2

---
+++ A2/Q1.java
@@ -0,0 +1,13 @@
+// You are an IT company's manager. Based on their performance over the last N working days, you must rate your employee. You are given an array of N integers called workload, where workload[i] represents the number of hours an employee worked on an ith day. The employee must be evaluated using the following criteria:
+// Rating = the maximum number of consecutive working days when the employee has worked more than 6 hours.
+// You are given an integer N where N represents the number of working days. You are given an integer array workload where workload[i] represents the number of hours an employee worked on an ith day.
+
+public class Q1 {
+    public static void main(String[] args) {
+        int[] workload = {8, 6, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int N = workload.length;
+
+        
+    }
+}
+
